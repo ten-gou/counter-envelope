4c7a5e6bf4096d1a675284995c247d0b81dd0bf9
Maribel <lwang79@zoho.com>
2022-06-09 22:03:07 -0700

    work has begun on the navbar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        contactSelected,
+        CurrentCategory,
+        setContactSelected
+    } = props;
+
+    //useEffects go here
+
+
+};
+
+export default Nav;
